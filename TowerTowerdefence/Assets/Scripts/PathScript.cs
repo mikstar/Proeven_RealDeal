3eb64fcea6152e3ca43ecdc1832958c5a1e72c2d
CoenMeijer <coennn@live.nl>
2016-04-26 14:11:46 +0200

    Art update commit
    Level art added

--- TowerTowerdefence/Assets/Scripts/PathScript.cs
+++ TowerTowerdefence/Assets/Scripts/PathScript.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 public class PathScript : MonoBehaviour {
 
-    public bool coen;
     public List<Transform> wpList = new List<Transform>();
     public bool showGizmo = true;
     public Color gizmoColor = Color.blue;
@@ -14,32 +13,14 @@ public class PathScript : MonoBehaviour {
         return list;
     }
     
-
     void OnDrawGizmos(){
-        if (showGizmo)
-        {
+        if (showGizmo){
             Gizmos.color = gizmoColor;
 
-            //~ if(Application.isPlaying){
-            //~ for(int i=1; i<wpSectionList.Count; i++){
-            //~ List<Vector3> subPathO=GetWPSectionPath(i-1);
-            //~ List<Vector3> subPath=GetWPSectionPath(i);
-
-            //~ //Debug.Log(i+"    "+wpSectionList[i].isPlatform+"    "+subPathO.Count+"   "+subPath.Count);
-
-            //~ Gizmos.DrawLine(subPathO[subPathO.Count-1], subPath[0]);
-            //~ for(int n=1; n<subPath.Count; n++){
-            //~ Gizmos.DrawLine(subPath[n-1], subPath[n]);
-            //~ }
-            //~ }
-            //~ }
-            //~ else{
-            for (int i = 1; i < wpList.Count; i++)
-            {
+            for (int i = 1; i < wpList.Count; i++){
                 if (wpList[i - 1] != null && wpList[i] != null)
                     Gizmos.DrawLine(wpList[i - 1].position, wpList[i].position);
             }
-            //~ }
         }
     }
 }
