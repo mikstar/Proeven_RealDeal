800fd9bba3d906940d53006e2f830600369962ec
CoenMeijer <coennn@live.nl>
2016-05-24 01:01:29 +0200

    Added enemy health GUI
    Added camera facing health bars for all current enemies.

--- TowerTowerdefence/Assets/Scripts/GUI/CameraFacingBillboard.cs
+++ TowerTowerdefence/Assets/Scripts/GUI/CameraFacingBillboard.cs
@@ -5,6 +5,11 @@ public class CameraFacingBillboard : MonoBehaviour {
 
     public Camera mainCam;
 
+
+    void Start() {
+        mainCam = Camera.main;
+    }
+
     void Update () {
         transform.LookAt(transform.position + mainCam.transform.rotation * Vector3.forward, mainCam.transform.rotation * Vector3.up);
     }
