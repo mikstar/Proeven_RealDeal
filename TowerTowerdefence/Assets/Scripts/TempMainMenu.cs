0813638c13fda87183e08a0c4091c2252da7c30b
CoenMeijer <coennn@live.nl>
2016-05-25 22:54:08 +0200

    Added temp main menu and game over screen

---
+++ TowerTowerdefence/Assets/Scripts/TempMainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempMainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame() {
+        Application.LoadLevel("ForestDay");
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+
+    public void SelectLevel() {
+
+    }
+}
